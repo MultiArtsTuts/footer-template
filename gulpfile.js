ccf7e9308e51fc237210a6ad019a8d9c6a28526b
multiarts <jmello@hotmail.com.br>
2016-07-02 04:39:40 -0300

    Gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,18 @@ var gulp = require("gulp"),//http://gulpjs.com/
 	minifycss = require('gulp-minify-css'),//https://www.npmjs.org/package/gulp-minify-css
 	rename = require('gulp-rename'),//https://www.npmjs.org/package/gulp-rename
 	log = util.log;
+
+// Dark Theme resource
 var darkScss = ['dark/resources/assets/footer-dark.scss'];
-gulp.task("sass", function(){
+
+// Light Theme resource
+var lightScss = ['light/resources/assets/footer-light.scss'];
+
+// Blue Theme resource
+var blueScss = ['blue/resources/assets/footer-blue.scss'];
+
+// Task Dark
+gulp.task("dark", function(){
 log("Generate CSS files " + (new Date()).toString());
 gulp.src(darkScss)
 	.pipe(sass({ style: 'expanded' }))
@@ -17,7 +27,31 @@ gulp.src(darkScss)
 	.pipe(gulp.dest('dark/css'));
 });
 
+// Task Light
+gulp.task("light", function(){
+log("Generate CSS files " + (new Date()).toString());
+gulp.src(lightScss)
+	.pipe(sass({ style: 'expanded' }))
+	.pipe(autoprefixer("last 3 version","safari 5", "ie 8", "ie 9"))
+	.pipe(gulp.dest("light/css"))
+	.pipe(rename({suffix: '.min'}))
+	.pipe(minifycss())
+	.pipe(gulp.dest('light/css'));
+});
+
+// Task Blue
+gulp.task("blue", function(){
+log("Generate CSS files " + (new Date()).toString());
+gulp.src(blueScss)
+	.pipe(sass({ style: 'expanded' }))
+	.pipe(autoprefixer("last 3 version","safari 5", "ie 8", "ie 9"))
+	.pipe(gulp.dest("blue/css"))
+	.pipe(rename({suffix: '.min'}))
+	.pipe(minifycss())
+	.pipe(gulp.dest('blue/css'));
+});
+
 gulp.task("watch", function(){
 	log("Watching scss files for modifications");
-	gulp.watch(darkScss, ["sass"]);
+	gulp.watch(darkScss, ["dark"]);
 });
