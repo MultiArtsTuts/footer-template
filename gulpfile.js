cc35f9ddf79d068d855de24efbf529a34d696303
multiarts <jmello@hotmail.com.br>
2016-07-02 03:44:17 -0300

    Dark Theme

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require("gulp"),//http://gulpjs.com/
+	util = require("gulp-util"),//https://github.com/gulpjs/gulp-util
+	sass = require("gulp-sass"),//https://www.npmjs.org/package/gulp-sass
+	autoprefixer = require('gulp-autoprefixer'),//https://www.npmjs.org/package/gulp-autoprefixer
+	minifycss = require('gulp-minify-css'),//https://www.npmjs.org/package/gulp-minify-css
+	rename = require('gulp-rename'),//https://www.npmjs.org/package/gulp-rename
+	log = util.log;
+var darkScss = ['dark/resources/assets/footer-dark.scss'];
+gulp.task("sass", function(){
+log("Generate CSS files " + (new Date()).toString());
+gulp.src(darkScss)
+	.pipe(sass({ style: 'expanded' }))
+	.pipe(autoprefixer("last 3 version","safari 5", "ie 8", "ie 9"))
+	.pipe(gulp.dest("dark/css"))
+	.pipe(rename({suffix: '.min'}))
+	.pipe(minifycss())
+	.pipe(gulp.dest('dark/css'));
+});
+
+gulp.task("watch", function(){
+	log("Watching scss files for modifications");
+	gulp.watch(darkScss, ["sass"]);
+});
